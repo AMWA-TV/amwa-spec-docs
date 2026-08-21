f68ca68afa9e60c3eb2e92bddfa1c3c074b64946
Peter Brightwell <peter.brightwell@bbc.co.uk>
2026-08-21 17:48:46 +0100

    Fix incorrect JSON render on historical fallback
    Signed-off-by: Peter Brightwell <peter.brightwell@bbc.co.uk>

--- scripts/prepare-docs.sh
+++ scripts/prepare-docs.sh
@@ -64,6 +64,10 @@ if not config.is_file():
         f"repo_url = {json.dumps(repo_url)}\n"
         'extra_css = ["stylesheets/extra.css"]\n'
         'extra_javascript = ["javascripts/json-viewer.js"]\n\n'
+        "[project.markdown_extensions.pymdownx.tabbed]\n"
+        "alternate_style = true\n\n"
+        "[project.markdown_extensions.toc]\n"
+        "permalink = false\n\n"
         "[project.extra.version]\n"
         'provider = "mike"\n',
         encoding="utf-8",
